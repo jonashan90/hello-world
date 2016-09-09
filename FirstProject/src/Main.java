e2de4d4138f87192c570b5fc5a1739a8e4539308
Jonas <Jonas@Jonas-PC>
2016-09-08 23:02:40 -0400

    First push.

---
+++ FirstProject/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		String name = "Jonas";
+		System.out.println("My name is " + name);
+	}
+
+}
